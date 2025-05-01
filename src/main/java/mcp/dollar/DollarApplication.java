31e8c3951ff292784858ec978f6e154a5824341a
PabloMusaber <musaberpablo@gmail.com>
2025-05-01 09:50:19 -0300

    initial commit

---
+++ src/main/java/mcp/dollar/DollarApplication.java
@@ -0,0 +1,13 @@
+package mcp.dollar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarApplication.class, args);
+	}
+
+}
